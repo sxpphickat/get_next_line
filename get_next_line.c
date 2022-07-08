dc9e9c2cc9da84e90e6d1cf11f5410e98ea1cba2
vipereir <zzvtrx@gmail.com>
2022-07-08 17:55:22 -0300

    aaaaa

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: sphh <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/07 22:17:04 by sphh              #+#    #+#             */
-/*   Updated: 2022/07/08 00:20:09 by sphh             ###   ########.fr       */
+/*   Updated: 2022/07/08 16:21:16 by vipereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,28 +17,32 @@
 
 char	*ft_strchr(const char *s, int c);
 char	*ft_strjoin(char const *s1, char const	*s2);
+char	*ft_strdup(const char	*s1);
+char	*ft_substr(char const	*s, unsigned int start, size_t len);
+size_t	ft_strlen_brkl(const char *s);
 
 
-	BUFFER_SIZE=16;
+	BUFFER_SIZE=4;
 
 char	*get_next_line(int	fd)
 {
-	static char		*temp;
-	char		*buf;
 	static char	*rest;
+	char		*buf;
+	char		*str;
 
 	buf = calloc(sizeof(char) * BUFFER_SIZE + 1, 1);
-	temp = "$";
+	read(fd, buf, BUFFER_SIZE);
+	str = ft_strdup(buf);
 
 	while (!ft_strchr(buf, '\n'))
 	{
-		temp = ft_strjoin(temp, buf);
 		read(fd, buf, BUFFER_SIZE);
+		str = ft_strjoin(str, buf);
 	}
-	temp = ft_strjoin(temp, "\n");
-	rest = buf;
-	free(buf);
-	return (temp);
+	str = ft_substr(str, 0, ft_strlen_brkl(str));
+	str = ft_strjoin(str, "\n");
+	rest = ft_strchr(buf, '\n');
+	return (str);
 }
 
 int	main(void)
@@ -47,6 +51,7 @@ int	main(void)
 	char	*str;
 
 	fd = open("./file.txt", O_RDONLY);
+	printf("%s", get_next_line(fd));
 	printf("%s", get_next_line(fd));
 	printf("%s", get_next_line(fd));
 	printf("%s", get_next_line(fd));
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: sphh <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/07 22:48:37 by sphh              #+#    #+#             */
-/*   Updated: 2022/07/08 00:08:59 by sphh             ###   ########.fr       */
+/*   Updated: 2022/07/08 16:18:49 by vipereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,18 @@ size_t	ft_strlen(const char *s)
 
 	i = 0;
 	while (s[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+size_t	ft_strlen_brkl(const char *s)
+{
+	int		i;
+
+	i = 0;
+	while (s[i] != '\n')
 	{
 		i++;
 	}
@@ -64,3 +76,48 @@ char	*ft_strjoin(char const *s1, char const	*s2)
 	return (str);
 }
 
+char	*ft_strdup(const char	*s1)
+{
+	char	*string;
+	size_t	i;
+
+	i = 0;
+	string = (char *)malloc(sizeof(*string) * (ft_strlen(s1) + 1));
+	if (string == NULL)
+		return (NULL);
+	while (s1[i])
+	{
+		string[i] = s1[i];
+		i++;
+	}
+	string[i] = '\0';
+	return (string);
+}
+
+char	*ft_substr(char const	*s, unsigned int start, size_t len)
+{
+	char	*str;
+	size_t	i;
+
+	i = 0;
+	if (!s)
+		return (NULL);
+	if (ft_strlen(s) < len)
+		len = ft_strlen(s);
+	str = (char *)malloc(len * sizeof(*str) + 1);
+	if (str == NULL)
+		return (NULL);
+	if (start >= ft_strlen(s))
+	{
+		str[i] = '\0';
+		return (str);
+	}
+	while (i < len)
+	{
+		str[i] = s[start];
+		start++;
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}

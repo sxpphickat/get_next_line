07556af0bea7992bdef631871a037c144357d37b
sphh <zzvtrx@gmail.com>
2022-07-09 12:32:38 -0300

    bug fix NULL return when error

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: sphh <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/07 22:17:04 by sphh              #+#    #+#             */
-/*   Updated: 2022/07/09 12:15:49 by sphh             ###   ########.fr       */
+/*   Updated: 2022/07/09 12:30:07 by sphh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,8 @@ char	*get_next_line(int	fd)
 		return (0);
 	buf = ft_calloc(sizeof(char) * BUFFER_SIZE + 1, 1);
 	read(fd, buf, BUFFER_SIZE);
+	if (buf[0] == 0)
+		return (0);
 	str = ft_strdup(buf);
 	while (!ft_strchr(buf, '\n') && !ft_find_eof(buf, BUFFER_SIZE))
 	{

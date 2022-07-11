ec30fe933dcc0b271c5f7d718836f812d6e46b89
vipereir <zzvtrx@gmail.com>
2022-07-11 18:37:26 -0300

    funcinadooooooo

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: sphh <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/07 22:17:04 by sphh              #+#    #+#             */
-/*   Updated: 2022/07/11 18:21:07 by vipereir         ###   ########.fr       */
+/*   Updated: 2022/07/11 18:37:18 by vipereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <stdio.h>
-#define BUFFER_SIZE 42
 
 char	*ft_strchr(const char *s, int c);
 char	*ft_strjoin(char const *s1, char const	*s2);
@@ -31,16 +30,16 @@ char	*get_next_line(int	fd)
 	char		*str;
 	int			eof;
 
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+
 	buf = ft_calloc(sizeof(char) * BUFFER_SIZE + 1, 1);
 	eof = 1;
 	while (eof)
 	{
 		eof = read(fd, buf, BUFFER_SIZE);
 		if (!eof && !rest)
-		{
-			printf("aaaaaaaaaaaaaaa");
 			return (NULL);
-		}
 		if (rest)
 		{
 			str = ft_strdup(rest);
@@ -58,8 +57,10 @@ char	*get_next_line(int	fd)
 			return (ft_substr(str, 0, ft_strlen_brkl(str) + 1));
 		}
 	}
+	return(NULL);
 }
 
+/*
 int	main(void)
 {
 	int	fd;
@@ -80,5 +81,10 @@ int	main(void)
 	printf("%s", get_next_line(fd));
 	printf("%s", get_next_line(fd));
 	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
 }
-
+*/
